76d30abb2a18b4ff2756d25243175a6e214af669
Ramakali <atman.ramakali@gmail.com>
2022-08-29 17:12:44 -0300

    search text and prefix implemented

--- user-input/index.js
+++ user-input/index.js
@@ -1,10 +1,18 @@
-import { question } from 'readline-sync'
+const readline = require('readline-sync')
 function start(){
     const content = {}
     content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
     function askAndReturnSearchTerm() {
-        return question("Type a Wikipedia search term: ")
+        return readline.question('Type a Wikipedia search term: ')
+    }
+    function askAndReturnPrefix(){
+        const prefixes =['Who is','What is', "The history of"]
+        const selectedPrefixIndex = readline.keyInSelect(prefixes,'Choose one option: ')
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+        return selectedPrefixText
     }
     console.log(content)
 }
+
 start()
